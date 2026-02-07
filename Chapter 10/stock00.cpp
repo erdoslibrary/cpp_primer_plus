402a210da955abdc5ab57f80b6858f316a9ef96d
erdoslibrary <book419@gmail.com>
2026-02-07 22:24:22 +0900

    feat(ch10): stock00 메인문 추가

--- Chapter 10/stock00.cpp
+++ Chapter 10/stock00.cpp
@@ -67,3 +67,30 @@ void Stock::show()
               << "  Share Price: $" << share_val
               << "  Total Worth: $" << total_val << '\n';
 }
+
+/* 메인문 */
+int main()
+{	// 1. 객체 생성(기본 생성자 혹은 초기화)
+	Stock sally;
+
+	std::cout <<"---초기 상태 ---" << std::endl;
+	sally.acquire("NanoSmart", 20, 12.50); // 초기 데이터 설정
+	sally.show();
+
+	// 2. 주식 추가 매수 (buy)
+	std::cout << "\n---15주 추가 매수(가격 $18.12)---" << std::endl;
+	sally.buy(15, 18.12);
+	sally.show();
+
+	// 3. 주식 매도(sell)
+	std::cout << "\n---40주 매도 시도(보유량보다 많음)---" <<std::endl;
+	sally.sell(40, 20.00); // 방어적 코드 확인
+	sally.show();
+
+	//4. 가격 갱신(update)
+	std::cout << "\n---주가 급등! ($40.00으로 갱신)---" << std::endl;
+	sally.update(40.00);
+	sally.show();
+
+	return 0;
+}
