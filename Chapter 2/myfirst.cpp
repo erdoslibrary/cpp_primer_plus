62711fcb449fd1e8055f0206a7d97db0d6375e99
erdoslibrary <book419@gmail.com>
2026-02-10 20:51:23 +0900

    chore(ch2): myfirst.cpp 체크

--- Chapter 2/myfirst.cpp
+++ Chapter 2/myfirst.cpp
@@ -5,11 +5,7 @@ int main()                                    // function header
 {                                             // start of function body
     using namespace std;                      // make definitions visible
     cout << "Come up and C++ me some time.";  // message
-    cout << endl;                             // start a new line
-    cout << "You won't regret it!" << endl;   // more output
-// If the output window closes before you can read it,
-// add the following code:
-    // cout << "Press any key to continue." <<endl;
-	// cin.get();                                                   
+    // cout << endl;                             // start a new line
+    cout << "You won't regret it!" << endl;   // more output                                                
     return 0;                                 // terminate main()
 }                                             // end of function body
