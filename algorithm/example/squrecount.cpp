3d6c3b5fc602b95a9a4e8ed5dfcaf3450b8c0068
erdoslibrary <book419@gmail.com>
2026-03-26 13:22:49 +0900

    chore: 메인문 수정

--- algorithm/example/squrecount.cpp
+++ algorithm/example/squrecount.cpp
@@ -9,11 +9,28 @@ const int COL = 4;
 
 int countSqures();
 
-int main() {
+// int main() {
+// 	int result = countSqures();
+// 	cout << "만들 수 있는 정사각형의 총 개수: " << result << endl;
+// 	return 0;
+// }
+
+/* 나는 매크로처럼 정의하지 않고 실행파일 인자로 받고 싶다. */
+int main(int argc, char **argv) {
+	if (argc < 3) {
+		cout << "사용법: " << argv[0] << " [ROW][COL]" << endl;
+		cout << "예시: " << argv[0] << " 3 4" << endl;
+		return 1;
+	}
+	int row = atoi(argv[1]);
+	int col = atoi(argv[2]);
+
 	int result = countSqures();
+	cout << "입력: " << row << " x " << col << endl;
 	cout << "만들 수 있는 정사각형의 총 개수: " << result << endl;
 	return 0;
 }
+
 
 int countSqures() {
 	vector<vector<int>> dp(ROW, vector<int>(COL, 1));
